9bc70e4755dc696d5685c885200b9ebd8455d242
secondarykey <secondarykey@gmail.com>
2026-07-01 04:59:09 +0900

    feat: manageから開発できるように変更

--- manage/vite.config.js
+++ manage/vite.config.js
@@ -9,4 +9,24 @@ export default defineConfig({
       '@mui/icons-material': '@mui/icons-material/esm',
     },
   },
+  server: {
+    proxy: {
+      '/manage/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+      },
+      '/login': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+      },
+      '/logout': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+      },
+      '/session': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+      },
+    },
+  },
 })
